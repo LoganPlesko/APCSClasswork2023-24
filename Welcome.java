97f5746ad56fbc13515db31e061ca7a76d9fe48e
Plesko <1lmplesko@seattleschools.org>
2023-10-02 09:32:29 -0700

    print practice

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+   public static void main(String[] args) {
+      System.out.println("Pikachu welcomes you to the world of Pokémon!");
+      System.out.println();
+      System.out.println("(\\__/)");
+      System.out.println("(o^.^)");
+      System.out.println("z(\")_(\")");
+      System.out.print("fsfsfsfsf");
+      System.out.print("dsshjkshkg");
+   }
+}    
+
