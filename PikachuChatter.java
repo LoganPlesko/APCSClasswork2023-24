e6042b1e665ef59faa6b0fd1a4dd9e214afd52cc
Plesko <1lmplesko@seattleschools.org>
2023-10-02 09:31:57 -0700

    prints pikachu art twice

---
+++ PikachuChatter.java
@@ -0,0 +1,16 @@
+//Logan Plesko, AP CS, Fall 2023, Pikachu chatter//
+public class PikachuChatter {
+   public static void main(String[] args) {
+      System.out.println("Pika? Pika pika pika chu peeeee ka chu!");
+      Pikachu();
+      System.out.println("Pika? Pika pika pika chu peeeee ka chu!");
+      Pikachu();
+}
+
+     public static void Pikachu() {
+         System.out.println("(\\__/)");
+         System.out.println("(o^.^)");
+         System.out.println("z(_(\")(\")");
+         
+   }
+}
