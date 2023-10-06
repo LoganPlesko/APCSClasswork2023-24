04b4aaa39dc327cdcb5da80f6b103dccb5bed6e0
Plesko <1lmplesko@seattleschools.org>
2023-10-06 09:37:37 -0700

    Staric methods for an object, classwork for AP CS.

---
+++ Bicycle.java
@@ -0,0 +1,25 @@
+//Logan Plesko, AP CS, Fall 2023, Create your own object class assignment
+public class Bicycle {
+    public static void main(String[] args) {
+      PedalForward();
+      PedalBackward();
+      DropKickstand();
+      RingBell();
+      Brake();
+  }
+   
+  //Pedal forward with the bike   
+   public static void PedalForward(){}
+   
+  //Pedal backward with the bike
+   public static void PedalBackward(){}
+  
+  //Set the bike down with the kickstant
+   public static void DropKickstand(){}
+  
+  //Ring the bell to let people know you're there
+   public static void RingBell(){}
+   
+  //Brake to slow down
+   public static void Brake(){}
+}      
